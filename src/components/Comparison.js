55806e46604a93e1ab87193094dfa7e801e13a6e
Tsebo Ramonyalioa <200200@virtualwindow.co.za>
2023-02-28 01:40:19 +0200

    API Call successful!!!
    Players are displayed in the console

--- src/components/Comparison.js
+++ src/components/Comparison.js
@@ -5,6 +5,26 @@ import { Link } from "react-router-dom";
 import { useEffect, useState } from "react";
 
 const Compare = () => {
+
+    const [api, setApi] = useState([]);
+
+    useEffect(() => {
+
+        axios.get('https://www.balldontlie.io/api/v1/players')
+        .then((res)=> {
+            setApi(res.data)
+            console.log(res)
+
+        })
+        .catch((err) => {
+            console.log(err)
+        })
+
+    }, [])
+
+
+
+
     return(
         <div className="nav-bar"></div>
     )
---
+++ src/components/SyntaxNotes.js
@@ -0,0 +1,37 @@
+// Axios Request 
+
+
+// import { useEffect } from "react"
+
+// axios.get(apilink)
+// .then((res) => {
+
+// })
+// .catch((err) => {
+
+// })
+
+
+// But Axios needs be in a useEffect
+
+// useEffect Syntax
+// useEffect(() => {
+
+// }, [])
+
+
+
+
+// Axios Request inside a useEffect
+
+// useEffect(() => {
+
+//     axios.get(apiLink)
+//     .then((res) => {
+
+//     })
+//     .catch((err) => {
+
+//     })
+    
+// }, [])
