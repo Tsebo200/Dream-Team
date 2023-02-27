25875a47367b890475ab67a947e65e065261668a
Tsebo Ramonyalioa <200200@virtualwindow.co.za>
2023-02-28 00:51:27 +0200

    cleaning up react app
    Deleting unnecessary files
    Installed React Router dom
    imported React Hooks (useSate & useEffect)

--- src/components/Comparison.js
+++ src/components/Comparison.js
@@ -1,9 +1,11 @@
 import axios from "axios";
+import { useEffect, useState } from "react";
 
-axios.get('https://balldontlie.io/api/v1/stats')
-.then((res) => {
-    console.log(res);
-})
-.catch((err) => {
-    console.log(err);  
-});
+
+// axios.get('https://balldontlie.io/api/v1/stats')
+// .then((res) => {
+//     console.log(res);
+// })
+// .catch((err) => {
+//     console.log(err);  
+// });
