50da444ab6e3e683109dcd3bb687294d492224f6
Tsebo Ramonyalioa <200200@virtualwindow.co.za>
2023-02-28 01:17:04 +0200

    app working
    But need to tidy folder and transition to jsx & saas

--- src/components/Comparison.js
+++ src/components/Comparison.js
@@ -1,5 +1,16 @@
+import React from "react";
 import axios from "axios";
+import './comparison.css';
+import { Link } from "react-router-dom";
 import { useEffect, useState } from "react";
+
+const Compare = () => {
+    return(
+        <div className="nav-bar"></div>
+    )
+}
+export default Compare
+
 
 
 // axios.get('https://balldontlie.io/api/v1/stats')
