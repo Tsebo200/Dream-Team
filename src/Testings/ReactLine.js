a2305e4d41c8ee1613e482d49661da90de7eecf5
Tsebo Ramonyalioa <200200@virtualwindow.co.za>
2023-02-28 03:36:27 +0200

    Line Graph now dispalying
    Remove faker and add actual data in an array

--- src/Testings/ReactLine.js
+++ src/Testings/ReactLine.js
@@ -1,11 +1,9 @@
 import React from 'react';
 import {Chart as ChartJS, CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend} from 'chart.js';
 import { Line } from 'react-chartjs-2';
-import faker from 'faker';
+// import faker from 'faker';
 
 const LineChart = () => {
-
-
 
 ChartJS.register(
   CategoryScale,
@@ -38,13 +36,15 @@ const data = {
   datasets: [
     {
       label: 'Dataset 1',
-      data: labels.map(() => faker.datatype.number({ min: -1000, max: 1000 })),
+      data:  [65, 59, 80, 81, 56, 55, 40],
+    // data: labels.map(() => faker.datatype.number({ min: -1000, max: 1000 })),
       borderColor: 'rgb(255, 99, 132)',
       backgroundColor: 'rgba(255, 99, 132, 0.5)',
     },
     {
       label: 'Dataset 2',
-      data: labels.map(() => faker.datatype.number({ min: -1000, max: 1000 })),
+      data:  [70, 2, 15, 50, 56, 55, 40],
+    // data: labels.map(() =>  faker.datatype.number({ min: -1000, max: 1000 })),
       borderColor: 'rgb(53, 162, 235)',
       backgroundColor: 'rgba(53, 162, 235, 0.5)',
     },
