cb42fb5cf1e5eda67a52cc7ef7ba4b12a45ffb5b
Tsebo Ramonyalioa <200200@virtualwindow.co.za>
2023-03-12 16:45:05 +0200

    Basic Vert Nav Done for now

--- src/Pages/Landing.jsx
+++ src/Pages/Landing.jsx
@@ -17,12 +17,16 @@ const Landing = () => {
            <div className="userIcon"></div>
         </div>
 
+    
         <div className="vertNavBar">
-
+            <div className="navSpacerOne"></div>
+            <br></br>
             <Link to="/"><h2><ion-icon name="basketball-outline"></ion-icon>Dashboard</h2></Link>
+            <br></br>
             <Link to="/compare"><h2> <ion-icon name="basketball-outline"></ion-icon>Comparison</h2></Link>
+            <br></br>
             <Link to="/time"><h2> <ion-icon name="basketball-outline"></ion-icon>Timeline</h2></Link>
-
+            <div className="navSpacerTwo"></div>
             <Link to="/signedout"><h2 className="signOutText"><ion-icon  name="log-out-outline"></ion-icon>Sign Out</h2></Link>
 
         </div>
