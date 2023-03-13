1c3646c9b604fd3b47c626697f22395bfd9230a8
Tsebo Ramonyalioa <200200@virtualwindow.co.za>
2023-03-13 12:40:12 +0200

    Created new sass files for different components
    Still To Do
    Adding introductory text
    Trying to add an image

--- src/Pages/Landing.jsx
+++ src/Pages/Landing.jsx
@@ -3,7 +3,6 @@ import axios from "axios";
 import { Link } from "react-router-dom";
 import { useEffect, useState } from "react";
 // import NavBar from './components/Navbar';
-
 import '../Sass/styles.sass';
 
 
@@ -36,25 +35,31 @@ const Landing = () => {
         {/* Introduction */}
         <div className="introContainer">
             <h2 className="introHeading">Introduction</h2>
-            <p className="introText">Lorem ipsum dolor sit amet consectetur. Fusce porttitor ut venenatis phasellus commodo sit <br></br> faucibus. In nulla enim faucibus aliquet quam pellentesque nunc neque. Urna morbi sed lectus 
-            malesuada ac facilisis. 
-            Risus purus sed vestibulum hendrerit duis malesuada scelerisque id fermentum.</p>
+            <center><p className="introText">Welcome fellow NBA viewer or 2k Gamer I present to you lovely ballers,</p> <h4>Dream Team.</h4></center><br></br>
+            <p className="introText"> Dream Team is an API driven React website that is based on an NBA API called BallDontLie, Dream Team has made NBA data consumption as easy as possible,
+            as it delivers the data with Chart.js which makes the site interactive for the visitor.
+            
+            Dream Team is where we deliver NBA Data as easy and consumable as possible for you to process with the aid of Chart.js. 
+            
+            Well let me explain how this all works out<br></br> 
+            
+            We hope you find the viewing experience of the NBA statistics fantastic and entertaining</p>
         </div>
         
 
         {/* Chart Section */}
-        <div className="chartsContainer">
+        {/* <div className="chartsContainer">
             <div className="chartOneContainer">
                 <div className="chartOne"></div>
             </div>
             <div className="chartTwoContainer">
                 <div className="chartTwo"></div>
             </div>
-        </div>
+        </div> */}
       
     {/* Main Content Div Close */}
     </div> 
-    
+
         // <div className="mainContainer">
         // {/* <div className="header">
         //     <div className="logo"></div>
@@ -68,7 +73,6 @@ const Landing = () => {
         // {/* Main Container closing */}
         // </div>
     );
-
 }
 export default Landing
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,6 @@
-import React from "react"
+import React from "react";
+import '../Sass/header.sass';
+// import '../Assets/Images/Morant.png'
 
 const Header = () => {
 return(
@@ -8,6 +10,7 @@ return(
     <div className="spacingOne"></div>
     <h1>Dream Team</h1>
     <div className="userIcon"></div>
+    <img src="../Assets/Images/Morant.png"></img>
     </div>
 
 );
