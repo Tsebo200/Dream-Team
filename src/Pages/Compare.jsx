472737f92124cdf10d7378b73abfd8b215dee8d0
Tsebo Ramonyalioa <200200@virtualwindow.co.za>
2023-03-20 23:50:04 +0200

    BallerName on the x-axis of the bar graph

--- src/Pages/Compare.jsx
+++ src/Pages/Compare.jsx
@@ -64,37 +64,6 @@ const data2 = {
 
 
 
-  const options = {
-    responsive: true,
-    plugins: {
-      legend: {
-        position: 'top',
-      },
-      title: {
-        display: true,
-        text: 'Chart.js Bar Chart',
-      },
-    },
-  };
-  
-  const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
-  
-  const data = {
-    labels,
-    datasets: [
-      {
-        label: 'Dataset 1',
-        data:  [70, 2, 15, 50, 56, 55, 40],
-        backgroundColor: 'rgba(255, 99, 132, 0.5)',
-      },
-      {
-        label: 'Dataset 2',
-        data: [65, 59, 80, 81, 56, 55, 40],
-        backgroundColor: 'rgba(53, 162, 235, 0.5)',
-      },
-    ],
-  };
-
   
 
 const Compare = ()=>{
@@ -107,9 +76,9 @@ const Compare = ()=>{
 
     axios.get('https://www.balldontlie.io/api/v1/players')
     .then((res) => {
-    //  setBallerName(res.data.data[4].first_name)
+     setBallerName(res.data.data[4].first_name)
     //  console.log({ballerName})
-     console.log(res.data.data)
+     console.log(ballerName)
     })
      .catch((err) =>{
        console.log(err)
@@ -118,6 +87,41 @@ const Compare = ()=>{
    }, [])
 
 
+
+
+
+  
+   const options = {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: 'top',
+      },
+      title: {
+        display: true,
+        text: 'Chart.js Bar Chart',
+      },
+    },
+  };
+  
+  const labels = [ballerName, 'February', 'March', 'April', 'May', 'June', 'July'];
+  
+  const data = {
+    labels,
+    datasets: [
+      {
+        label: 'Dataset 1',
+        data:  [],
+        backgroundColor: 'rgba(255, 99, 132, 0.5)',
+      },
+      {
+        label: 'Dataset 2',
+        data: [65, 59, 80, 81, 56, 55, 40],
+        backgroundColor: 'rgba(53, 162, 235, 0.5)',
+      },
+    ],
+  };
+ 
 
 
     return(
