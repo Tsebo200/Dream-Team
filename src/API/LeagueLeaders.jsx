ef3acde590ed3e2944160e0d0680e782937b6e33
Tsebo Ramonyalioa <200200@virtualwindow.co.za>
2023-04-01 10:40:11 +0200

    Added the player Urls
    Deleted unneeded comments

---
+++ src/API/LeagueLeaders.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import axios from 'axios';
+import { useState, useEffect, useRef } from 'react';
+
+
+function LeagueLeaders() {
+const KD = "https://www.balldontlie.io/api/v1/stats?season[]=2018&player_ids[]=140";
+const Giannis = "https://www.balldontlie.io/api/v1/stats?season[]=2018&player_ids[]=15";
+const Jokic = 'https://www.balldontlie.io/api/v1/stats?season[]=2018&player_ids[]=246';
+const Young = 'https://www.balldontlie.io/api/v1/stats?season[]=2018&player_ids[]=490';
+const Curry = 'https://www.balldontlie.io/api/v1/stats?season[]=2018&player_ids[]=115';
+const Lavine = 'https://www.balldontlie.io/api/v1/stats?season[]=2018&player_ids[]=268';
+const Tatum = 'https://www.balldontlie.io/api/v1/stats?season[]=2018&player_ids[]=434';
+const DeRozan = 'https://www.balldontlie.io/api/v1/stats?season[]=2018&player_ids[]=125';
+const Luka = 'https://www.balldontlie.io/api/v1/stats?season[]=2018&player_ids[]=132';
+const George = 'https://www.balldontlie.io/api/v1/stats?season[]=2018&player_ids[]=172';
+
+
+  return (
+    
+    <div>LeagueLeaders</div>
+  )
+}
+
+export default LeagueLeaders
--- src/components/Charts/BarChart.jsx
+++ src/components/Charts/BarChart.jsx
@@ -10,32 +10,13 @@ function BarChart() {
 
     const [ballerData, setBallerData] = useState('')
 
-
-
-    // useEffect(() => {
-  
-    //   axios.get('https://www.balldontlie.io/api/v1/players')
-    //   .then((res) => {
-    //    setBallerName(res.data.data[4].first_name)
-    //   //  console.log({ballerName})
-    //    console.log(ballerName)
-    //   })
-    //    .catch((err) =>{
-    //      console.log(err)
-    //   })
      
-    //  }, [])
-   
-  
      useEffect(() => {
   
      axios.get('https://www.balldontlie.io/api/v1/stats')
      .then((res) => {
         let data = res.data.data 
-  
-  
-      // setBallerData(res.data.data[10].ast)
-      // setBallerData(res.data.data[24].ast)
+
   
   
       const fieldGoalsAttempted = []
@@ -45,16 +26,7 @@ function BarChart() {
       }
       console.log(fieldGoalsAttempted)
   
-      
-  
-      // setBallerData(res.data.data[0].fga)
-      
-      
-      // let playerStats = 
-      // setBallerData(res.data)
-      // playerStats(res.data)
-     //  console.log({ballerName})
-      // ballerData.map((data) => res.data.data),
+    
   
       console.log(ballerData)
      })
